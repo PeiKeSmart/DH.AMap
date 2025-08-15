74d8d5749c07acde5f5f8ce0dc5cc79002ce90a7
猿人易 <qcjxberin@outlook.com>
2025-08-15 17:39:28 +0800

    添加高德地图设置和启动配置类
    在 `AMapSetting.cs` 中新增 `AMapSetting` 类，包含高德地图密钥属性 `AMapSecret`，并使用相关特性进行标注。
    
    在 `DHStartup.cs` 中实现 `DHStartup` 类，配置服务和中间件，新增 `UseDHEndpoints` 方法以处理高德地图 API 请求，并附加密钥。

---
+++ DH.AMap/AMapSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace DH.AMap;
+
+/// <summary>高德地图设置</summary>
+[DisplayName("高德地图设置")]
+[Config("AMapSetting")]
+public class AMapSetting : Config<AMapSetting>
+{
+    /// <summary>
+    /// 高德地图密钥
+    /// </summary>
+    [Description("高德地图密钥")]
+    public String? AMapSecret { get; set; }
+}
